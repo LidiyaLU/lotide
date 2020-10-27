7f4d98411642fde23094a9864deb3d246b89169a
LidiyaLU <cheerrlidiya@yandex.ru>
2020-10-27 20:18:24 +0000

    create function to check if 2 arrays are equal and print corresponding message

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual,expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`😃 Arrays are equal: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`😡 Arrays are NOT equal: [${actual}] !== [${expected}]`);
+  }
+}
+
+assertArraysEqual(['hi','how','are','you'],['hi','how', 'are','you']);
+assertArraysEqual([1,2,3,4],[1,2,3]);
