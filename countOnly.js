43c6b9be5b601b50a46e4dc72b777c9bb535c72b
LidiyaLU <cheerrlidiya@yandex.ru>
2020-10-28 19:05:47 +0000

    create function to filter and count properties of object by elements of array

---
+++ countOnly.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😃😃Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👿👿Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const result = {};
+
+  for (const item of allItems) {
+
+    if (itemsToCount[item]) {
+      if (result[item]) {
+        result[item] += 1;
+      } else {
+        result[item] = 1;
+      }
+    }
+  }
+
+  return result;
+};
+
+
+
+//TEST
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+console.log(result1);
