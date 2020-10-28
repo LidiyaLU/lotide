b6d7ed9470a486579fd5dfbebd4dae12ee38d7e0
LidiyaLU <cheerrlidiya@yandex.ru>
2020-10-28 00:29:02 +0000

    create function to unpack array within array and add assertion

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual,expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`😃 Arrays are equal: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`😡 Arrays are NOT equal: [${actual}] !== [${expected}]`);
+  }
+};
+
+const flatten = function(arr) {
+  let flat = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        flat.push(arr[i][j]);
+      }
+    } else {
+      flat.push(arr[i]);
+    }
+  }
+  return flat;
+};
+
+const testQ = [1,2,[3,4],[5,6]];
+assertArraysEqual(flatten(testQ),[1,2,3,4,5,6]);
+assertArraysEqual(testQ, flatten(testQ));
